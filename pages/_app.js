7f527f5ff4a640b8be084765628c3fc35323f849
Ahmad Alfan <aalf4nz@gmail.com>
2021-11-16 20:34:40 +0700

    Change tailwind connection & font family

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import 'tailwindcss/tailwind.css';
+import Head from 'next/head';
+import '../styles/global.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
@@ -13,10 +14,24 @@ const queryClient = new QueryClient({
 
 function MyApp({ Component, pageProps }) {
     return (
-        <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
-            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-        </QueryClientProvider>
+        <>
+            <Head>
+                <link rel='preconnect' href='https://fonts.googleapis.com' />
+                <link
+                    rel='preconnect'
+                    href='https://fonts.gstatic.com'
+                    crossOrigin
+                />
+                <link
+                    href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+                    rel='stylesheet'
+                />
+            </Head>
+            <QueryClientProvider client={queryClient}>
+                <Component {...pageProps} />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </>
     );
 }
 
