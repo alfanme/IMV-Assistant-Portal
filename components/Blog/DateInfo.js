5a1b224b3e399c6f0c9b0e8efdf350c2ffde9cb4
Ahmad Alfan <aalf4nz@gmail.com>
2021-11-16 20:32:00 +0700

    Add date info component to format DB timestamp

---
+++ components/Blog/DateInfo.js
@@ -0,0 +1,21 @@
+import { CalendarIcon } from '@heroicons/react/solid';
+
+export default function DateInfo({ timestamp }) {
+    const formatedDate = () => {
+        const date = new Date(timestamp);
+        const options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        };
+        return date.toLocaleDateString('en-US', options);
+    };
+
+    return (
+        <div className='flex gap-1 items-center'>
+            <CalendarIcon className='w-4 h-4 text-pink-500' />
+            <p className='text-xs text-pink-500'>{formatedDate()}</p>
+        </div>
+    );
+}
