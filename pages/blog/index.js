28db3396feb5a583c732ec0b74649e08342f1a71
Ahmad Alfan <aalf4nz@gmail.com>
2021-11-16 20:35:10 +0700

    Change blog main page for latest blogs

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,22 @@
 import Layout from '../../components/Layout';
+import BlogLayout from '../../components/Blog/Layout';
+import { useAllBlog } from '../../hooks/useBlog';
+import BlogCard from '../../components/Blog/Card';
+import Loading from '../../components/Loading';
 
 export default function index() {
+    const { data: allBlogData, isLoading } = useAllBlog();
+
+    if (isLoading) return <Loading />;
     return (
         <Layout>
-            <h1>Blog Page</h1>
+            <BlogLayout>
+                <div className='flex flex-col gap-8'>
+                    {allBlogData?.map((data, idx) => (
+                        <BlogCard key={idx} blogData={data} />
+                    ))}
+                </div>
+            </BlogLayout>
         </Layout>
     );
 }
