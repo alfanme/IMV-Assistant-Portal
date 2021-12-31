053964970eb5ab18ca6421c83422953e086cf5b4
Ahmad Alfan <aalf4nz@gmail.com>
2021-12-31 12:52:14 +0700

    Change icon size for mobile screen

--- components/Blog/Tab.js
+++ components/Blog/Tab.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import { NewspaperIcon, StarIcon, PencilIcon } from '@heroicons/react/solid';
 
-const iconStyle = 'w-4 h-4';
+const iconStyle = 'w-6 h-6 md:w-4 md:h-4';
 
 const tabMenus = [
     {
