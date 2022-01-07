77f8d9cc1fa6a4520c94a767edb9e0728e9e4e8d
Ahmad Alfan <aalf4nz@gmail.com>
2022-01-07 10:36:25 +0700

    Add date info and refine the layout

--- components/Webinar/ListItem.js
+++ components/Webinar/ListItem.js
@@ -1,6 +1,7 @@
 import { PencilAltIcon } from '@heroicons/react/solid';
 import Switch from 'react-switch';
 import { useToggleOpenRegistration } from '../../hooks/useWebinars';
+import DateInfo from '../DateInfo';
 
 export default function ListItem({ idx, webinar, setForm }) {
     const toggleOpenRegistrationMutation = useToggleOpenRegistration(
@@ -13,26 +14,33 @@ export default function ListItem({ idx, webinar, setForm }) {
     };
 
     return (
-        <div className='flex gap-2 md:gap-4 items-center px-4 md:px-8 h-16 md:h-20 bg-white rounded-xl shadow-md'>
+        <div className='flex gap-4 items-center px-4 md:px-8 py-4 bg-white rounded-xl shadow-md'>
             <p className='font-semibold'>{idx + 1}.</p>
-            <p className='flex-1 font-semibold truncate'>{webinar.title}</p>
-            <button
-                onClick={() => {
-                    setForm({
-                        show: true,
-                        editMode: true,
-                        data: webinar,
-                        title: 'Edit webinar',
-                    });
-                }}
-                className='flex gap-2 items-center mr-4 px-2 md:px-4 py-2 bg-white text-orange-500 rounded-md shadow-lg font-semibold'>
-                <PencilAltIcon className='w-4 h-4' />
-                <p className='hidden md:block'>Edit</p>
-            </button>
-            <Switch
-                checked={webinar.open_registration}
-                onChange={handleSwitch}
-            />
+            <div className='flex-1'>
+                <p className='font-semibold whitespace-normal mb-1'>
+                    {webinar.title}
+                </p>
+                <DateInfo timestamp={webinar.held_on} />
+            </div>
+            <div className='flex flex-col-reverse md:flex-row items-center gap-4'>
+                <button
+                    onClick={() => {
+                        setForm({
+                            show: true,
+                            editMode: true,
+                            data: webinar,
+                            title: 'Edit webinar',
+                        });
+                    }}
+                    className='flex gap-2 items-center px-2 md:px-4 py-2 bg-white text-orange-500 rounded-md shadow-lg font-semibold'>
+                    <PencilAltIcon className='w-4 h-4' />
+                    <p className=''>Edit</p>
+                </button>
+                <Switch
+                    checked={webinar.open_registration}
+                    onChange={handleSwitch}
+                />
+            </div>
         </div>
     );
 }
